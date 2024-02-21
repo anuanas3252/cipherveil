00d1301c6b552dd7073ae4e126ff64645bdc5010
anuanas3252 <anuanas3252@gmail.com>
2024-02-21 23:00:28 +0530

    created a 2 new apps named 'contactapp' and 'steganoapp'

--- cipherveil/cipherveil/settings.py
+++ cipherveil/cipherveil/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'frontlineapp',
-    'contact',
+    'contactapp',
+    'steganoapp',
 ]
 
 MIDDLEWARE = [

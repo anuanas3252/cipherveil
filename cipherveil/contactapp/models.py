81016bde858250f60e49163a860fd3c4d8e635b6
anuanas3252 <anuanas3252@gmail.com>
2024-03-23 12:39:11 +0530

    ContactBook & OpenID model created and migrated

--- cipherveil/contactapp/models.py
+++ cipherveil/contactapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class ContactBook(models.Model):
+    tag = models.CharField(max_length=255,unique=True,blank=False,null=False)
+    name = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+    
+
+def generate_alphanumeric_id():
+    unique_id = str(uuid.uuid4().hex)
+    return unique_id
+
+class OpenID(models.Model):
+    tag = models.CharField(max_length=255,unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.contactID:
+            self.contactID = generate_alphanumeric_id()
+        super(OpenID, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.contactID

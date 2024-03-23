cbcf66fc93385df92c73d380e5a6966a7b34647b
anuanas3252 <anuanas3252@gmail.com>
2024-03-23 13:17:48 +0530

    Update admin.py
    added the code to view contactbook and openid in admin site

--- cipherveil/contactapp/admin.py
+++ cipherveil/contactapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ContactBook, OpenID
+
+class ContactBookAdmin(admin.ModelAdmin):
+    list_display = ('id','name','tag')
+    #search_fields = ('id','name','tag')
+
+    
+    
+
+class OpenIDAdmin(admin.ModelAdmin):
+    list_display = ('id','tag')
+    #search_fields = ('id','tag')
+
+
+
+admin.site.register(OpenID,OpenIDAdmin)
+admin.site.register(ContactBook,ContactBookAdmin)

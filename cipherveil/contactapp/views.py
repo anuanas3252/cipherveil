d200179fde481f33b8336af03f444bc1f3db2777
anuanas3252 <anuanas3252@gmail.com>
2024-03-23 15:03:52 +0530

    added the code for restricting the unauthorised users

--- cipherveil/contactapp/views.py
+++ cipherveil/contactapp/views.py
@@ -3,59 +3,82 @@ from .models import ContactBook
 
 # Create your views here.
 def add_contact(request):
-    error_message = None
-    if request.method == 'POST':
-        # Get data from the request
-        tag = request.POST.get('tag')
-        name = request.POST.get('name')
+    
+    if request.user.is_authenticated:
+        
+        error_message = None
+        if request.method == 'POST':
+            # Get data from the request
+            tag = request.POST.get('tag')
+            name = request.POST.get('name')
 
-        # Create a new ContactBook instance
-        new_contact = ContactBook(tag=tag, name=name)
+            # Create a new ContactBook instance
+            new_contact = ContactBook(tag=tag, name=name)
 
-        try:
-            # Save the new contact to the database
-            new_contact.save()
-            return redirect('view_contact')  # Redirect to a view displaying the list of contacts
-        except Exception as e:
-            error_message = f"Error: {str(e)}"
-            #return render(request, 'error_page.html', {'error_message': error_message})
+            try:
+                # Save the new contact to the database
+                new_contact.save()
+                return redirect('view_contact')  # Redirect to a view displaying the list of contacts
+            except Exception as e:
+                error_message = f"Error: {str(e)}"
+                #return render(request, 'error_page.html', {'error_message': error_message})
 
-    return render(request, 'contactapp/add_contact.html', {'error_message': error_message})
+        return render(request, 'contactapp/add_contact.html', {'error_message': error_message})
+    
+    else:
+        # Redirect to the signin page if the user is not authenticated
+        return redirect('signin')
 
 
 def view_contact(request):
-    error_message = None
-    contacts = ContactBook.objects.all()
-    return render(request, 'contactapp/view_contact.html', {'contacts': contacts})
+    if request.user.is_authenticated:
+        error_message = None
+        contacts = ContactBook.objects.all()
+        return render(request, 'contactapp/view_contact.html', {'contacts': contacts})
+
+    else:
+        # Redirect to the signin page if the user is not authenticated
+        return redirect('signin')
 
 
 def edit_contact(request, pk):
-    error_message = None
-    contact = get_object_or_404(ContactBook, pk=pk)
+    if request.user.is_authenticated:
+        error_message = None
+        contact = get_object_or_404(ContactBook, pk=pk)
 
-    if request.method == 'POST':
-        # Update the contact details with the submitted form data
-        contact.tag = request.POST['tag']
-        contact.name = request.POST['name']
-        contact.save()
-
-        try:
-            # Save the new contact to the database
+        if request.method == 'POST':
+            # Update the contact details with the submitted form data
+            contact.tag = request.POST['tag']
+            contact.name = request.POST['name']
             contact.save()
-            return redirect('view_contact')  # Redirect to a view displaying the list of contacts
-        except Exception as e:
-            error_message = f"Error: {str(e)}"
 
-    return render(request, 'contactapp/edit_contact.html', {'contact': contact, 'error_message': error_message})
+            try:
+                # Save the new contact to the database
+                contact.save()
+                return redirect('view_contact')  # Redirect to a view displaying the list of contacts
+            except Exception as e:
+                error_message = f"Error: {str(e)}"
+
+        return render(request, 'contactapp/edit_contact.html', {'contact': contact, 'error_message': error_message})
+    
+    else:
+        # Redirect to the signin page if the user is not authenticated
+        return redirect('signin')
+    
 
 def delete_contact(request, pk):
-    # Get the contact instance
-    contact = get_object_or_404(ContactBook, pk=pk)
+    if request.user.is_authenticated:
+        # Get the contact instance
+        contact = get_object_or_404(ContactBook, pk=pk)
 
-    if request.method == 'POST':
-        # Delete the contact
-        contact.delete()
-        # Redirect to a success page or any other appropriate page
-        return redirect('view_contact')
+        if request.method == 'POST':
+            # Delete the contact
+            contact.delete()
+            # Redirect to a success page or any other appropriate page
+            return redirect('view_contact')
 
-    return render(request, 'contactapp/delete_contact.html', {'contact': contact})
+        return render(request, 'contactapp/delete_contact.html', {'contact': contact})
+    
+    else:
+        # Redirect to the signin page if the user is not authenticated
+        return redirect('signin')
--- cipherveil/steganoapp/views.py
+++ cipherveil/steganoapp/views.py
@@ -13,72 +13,88 @@ import base64
 # Create your views here.
 
 def steganoencode(request):
-    if request.method == 'POST':
-        
-        encrypted_text_str = request.session.get('encrypted_text_str', None)
-        print("5) CIPHERTEXT STR = ",encrypted_text_str,"   END")
-        if encrypted_text_str is None:
-            # Redirect to keygenerate if the key is not available
-            return redirect('encryptionkeygenerate')  # Adjust the URL name as needed
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            
+            encrypted_text_str = request.session.get('encrypted_text_str', None)
+            print("5) CIPHERTEXT STR = ",encrypted_text_str,"   END")
+            if encrypted_text_str is None:
+                # Redirect to keygenerate if the key is not available
+                return redirect('encryptionkeygenerate')  # Adjust the URL name as needed
+
+            
+            Encoded_Image_File_Name = request.POST.get('Encoded_Image_File_Name', '')
+            image = request.FILES.get('image')
+
+            # Open the image
+            img = Image.open(image)
+
+            
+            
+            encoded_image = lsb.hide(img, encrypted_text_str)
+
+            # Save the encoded image
+            Encoded_Image_File_Name = f'{Encoded_Image_File_Name}.png'
+            encoded_image_path = f'media/{Encoded_Image_File_Name}'
+            encoded_image.save(encoded_image_path)
+
 
         
-        Encoded_Image_File_Name = request.POST.get('Encoded_Image_File_Name', '')
-        image = request.FILES.get('image')
 
-        # Open the image
-        img = Image.open(image)
+            return render(request, 'steganoapp/steganoencode.html', {'Encoded_Image_File_Name':Encoded_Image_File_Name})
 
-        
-        
-        encoded_image = lsb.hide(img, encrypted_text_str)
-
-        # Save the encoded image
-        Encoded_Image_File_Name = f'{Encoded_Image_File_Name}.png'
-        encoded_image_path = f'media/{Encoded_Image_File_Name}'
-        encoded_image.save(encoded_image_path)
-
-
-       
-
-        return render(request, 'steganoapp/steganoencode.html', {'Encoded_Image_File_Name':Encoded_Image_File_Name})
-
-    return render(request, 'steganoapp/steganoencode.html')
+        return render(request, 'steganoapp/steganoencode.html')
+    
+    else:
+        # Redirect to the signin page if the user is not authenticated
+        return redirect('signin')
 
 
 def download_image_view(request, image_name):
-    # Assuming your images are stored in a folder named 'images' within your media directory
-    image_path = os.path.join(settings.MEDIA_ROOT, image_name)
+    if request.user.is_authenticated:
+        # Assuming your images are stored in a folder named 'images' within your media directory
+        image_path = os.path.join(settings.MEDIA_ROOT, image_name)
 
-    if os.path.exists(image_path):
-        with open(image_path, 'rb') as image_file:
-            response = HttpResponse(image_file.read(), content_type='image/png')  # Adjust content type based on your image format
-            response['Content-Disposition'] = 'attachment; filename={}'.format(image_name)
-            return response
+        if os.path.exists(image_path):
+            with open(image_path, 'rb') as image_file:
+                response = HttpResponse(image_file.read(), content_type='image/png')  # Adjust content type based on your image format
+                response['Content-Disposition'] = 'attachment; filename={}'.format(image_name)
+                return response
+        else:
+            return HttpResponse("Image not found", status=404)
+    
     else:
-        return HttpResponse("Image not found", status=404)
+        # Redirect to the signin page if the user is not authenticated
+        return redirect('signin')
     
     
     
 
 def steganodecode(request):
-    if request.method == 'POST':
-        image = request.FILES.get('image')
+    
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            image = request.FILES.get('image')
 
-        # Open the encoded image
-        encoded_img = Image.open(image)
+            # Open the encoded image
+            encoded_img = Image.open(image)
 
-        # Retrieve the hidden binary text using LSB
-        # binary_text = retrieve_text(encoded_img)
+            # Retrieve the hidden binary text using LSB
+            # binary_text = retrieve_text(encoded_img)
 
-        # Convert binary text to ASCII
-        # text = ''.join(chr(int(binary_text[i:i + 8], 2)) for i in range(0, len(binary_text), 8))
+            # Convert binary text to ASCII
+            # text = ''.join(chr(int(binary_text[i:i + 8], 2)) for i in range(0, len(binary_text), 8))
 
-        decoded_data = lsb.reveal(encoded_img)
-        print("Decoded text = encrypted_text_str @ steganodecode =",decoded_data)
-        request.session['decryption_text_str'] = decoded_data
-        
-        print("6)) CIPHERTEXT STR = ",decoded_data,"   END")
-        
-        #return render(request, 'steganoapp/steganodecode.html', {'decoded_text': decoded_data})
-        return redirect('decryptionkeygenerate')
-    return render(request, 'steganoapp/steganodecode.html')
+            decoded_data = lsb.reveal(encoded_img)
+            print("Decoded text = encrypted_text_str @ steganodecode =",decoded_data)
+            request.session['decryption_text_str'] = decoded_data
+            
+            print("6)) CIPHERTEXT STR = ",decoded_data,"   END")
+            
+            #return render(request, 'steganoapp/steganodecode.html', {'decoded_text': decoded_data})
+            return redirect('decryptionkeygenerate')
+        return render(request, 'steganoapp/steganodecode.html')
+    
+    else:
+        # Redirect to the signin page if the user is not authenticated
+        return redirect('signin')

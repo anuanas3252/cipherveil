7e7b694bdbf168b03d6171f981f080be5c227bb2
anuanas3252 <anuanas3252@gmail.com>
2024-03-23 13:31:23 +0530

    Update urls.py
    **Changes made in cipherveil's urls.py file :**
    - newly added urls for
        1. contactapp

--- cipherveil/cipherveil/urls.py
+++ cipherveil/cipherveil/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontlineapp.urls')),
-    path('steganograpghy/', include('steganoapp.urls'))
+    path('steganograpghy/', include('steganoapp.urls')),
+    path('contact/', include('contactapp.urls')),
 ]

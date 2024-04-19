b4967d344c916ac22323bd70f692645c1f551dc6
anuanas3252 <anuanas3252@gmail.com>
2024-04-20 00:08:45 +0530

    minor changes

--- cipherveil/cryptoapp/views.py
+++ cipherveil/cryptoapp/views.py
@@ -1,37 +1,26 @@
 from django.shortcuts import render,redirect
-
 from contactapp.models import ContactBook
 from .crypto_code import derive_key,encrypt,decrypt
 import base64
 from decouple import config
-
 from django.contrib import messages
 
 def encryptionkeygenerate(request):
-    
     if request.user.is_authenticated:
         contacts = ContactBook.objects.all()
-
         if request.method == 'POST':
             selected_tag = request.POST.get('tag')
-            password = request.POST.get('password')
-            
+            password = request.POST.get('password') 
             print("TAG @ENC = ",selected_tag)
             print("PAS @ENC = ",password)
-            
             encryption_key = derive_key(password=password, salt=selected_tag.encode())
             print("1) KEY =",encryption_key,"   END")
-            
             print("KEY @ENC = ",encryption_key)
-            
             encryption_key_str = base64.b64encode(encryption_key).decode('utf-8')
             request.session['encryption_key_str'] = encryption_key_str
-            
             return redirect('messageencrypt')
         return render(request, 'cryptoapp/encryptionkeygenerate.html', {'contacts': contacts})
-    
     else:
-        # Redirect to the signin page if the user is not authenticated
         return redirect('signin')
 
 
@@ -39,32 +28,18 @@ def messageencrypt(request):
     if request.user.is_authenticated:
         if request.method == 'POST':
             plaintext = request.POST.get('text', '')
-
-
-            encryption_key_str = request.session.get('encryption_key_str', None)
-            
+            encryption_key_str = request.session.get('encryption_key_str', None)   
             if encryption_key_str is None:
-                # Redirect to encryptionkeygenerate if the key is not available
                 return redirect('encryptionkeygenerate')  # Adjust the URL name as needed
-
-            # Convert base64 encoded string back to bytes
             encryption_key = base64.b64decode(encryption_key_str)
-            
             print("2) KEY =",encryption_key,"   END")
-            
-            # Use the key for encryption
             encrypted_text = encrypt(plaintext.encode(), encryption_key)
-
             print("3) CIPHERTEXT = ",encrypted_text,"   END")
-            
             encrypted_text_str = base64.b64encode(encrypted_text).decode('utf-8')
             request.session['encrypted_text_str'] = encrypted_text_str
-
             print("4) CIPHERTEXT STR = ",encrypted_text_str,"   END")
-            
             return redirect('steganoencode')
         return render(request, 'cryptoapp/messageencrypt.html')
-    
     else:
         # Redirect to the signin page if the user is not authenticated
         return redirect('signin')
@@ -72,72 +47,42 @@ def messageencrypt(request):
 
 def decryptionkeygenerate(request):
     if request.user.is_authenticated:
-    
         decryption_text_str = request.session.get('decryption_text_str', None)
-            
         if decryption_text_str is None:
-            # Redirect to encryptionkeygenerate if the key is not available
             return redirect('steganodecode')  # Adjust the URL name as needed
-
-        
-        
         user_tag = config('USER_TAG', default='')
         print("USER TAG = ",user_tag)
-
         if request.method == 'POST':
             password = request.POST.get('password')
-            
             print("TAG @DEC = ",user_tag)
             print("PAS @DEC = ",password)
-            
             decryption_key = derive_key(password=password, salt=user_tag.encode())
             print("8)) KEY =",decryption_key,"   END")
-            
             print("KEY @DEC = ",decryption_key)
-            
             decryption_key_str = base64.b64encode(decryption_key).decode('utf-8')
             request.session['decryption_key_str'] = decryption_key_str
             print("9)) KEY STR=",decryption_key_str,"   END")
-            
             return redirect('messagedecrypt')
         return render(request, 'cryptoapp/decryptionkeygenerate.html', {})
     
     else:
-        # Redirect to the signin page if the user is not authenticated
         return redirect('signin')
 
 
 def messagedecrypt(request):
-    
     if request.user.is_authenticated:
-    
         decryption_text_str = request.session.get('decryption_text_str', None)
         decryption_key_str = request.session.get('decryption_key_str', None)
-        
         print("10)) CIPHERTEXT STR = ",decryption_text_str,"   END")
         print("11)) KEY STR=",decryption_key_str,"   END")
-        
-    
-        
         if decryption_text_str is None or decryption_key_str is None:
-            # Redirect to encryptionkeygenerate if the key is not available
             return redirect('steganodecode')  # Adjust the URL name as needed
-        
-        
         decryption_key = base64.b64decode(decryption_key_str)
         decryption_text = base64.b64decode(decryption_text_str)
-        
         print("12)) CIPHERTEXT = ",decryption_text,"   END")
         print("13)) KEY =",decryption_key,"   END")
-        
-
-    
         decrypted_message = decrypt(decryption_text[28:], decryption_key, decryption_text[12:28], decryption_text[:12])
         print(decrypted_message.decode())
-        
-        
         return render(request, 'cryptoapp/messagedecrypt.html', {'decrypted_message':decrypted_message.decode()})
-    
     else:
-        # Redirect to the signin page if the user is not authenticated
         return redirect('signin')
--- cipherveil/frontlineapp/views.py
+++ cipherveil/frontlineapp/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-# Create your views here.
-
 from django.contrib import messages
-
 
 def index(request):
     return render(request, 'frontlineapp/index.html', {'page': 'index'})
@@ -12,38 +9,25 @@ def index(request):
 def about(request):
     return render(request, 'frontlineapp/about.html', {'page': 'about'})
 
-
-
-
-
 def home(request):
-    # Check if the user is authenticated
     if request.user.is_authenticated:
         return render(request, 'frontlineapp/home.html', {'page': 'home'})
     else:
-        # Redirect to the signin page if the user is not authenticated
         return redirect('signin')
-
-
 
 def signin(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # Redirect to a success page or home page
             return redirect('home')
         else:
             messages.error(request, 'Invalid login credentials')
     return render(request, 'frontlineapp/signin.html', {'page': 'signin'})
 
-
-
 def signout(request):
-    # Clear the session and log the user out
     logout(request)
     return redirect('signin')
 
